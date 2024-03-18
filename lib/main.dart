9fb88b9a9e4cf85805c28ef16b1de74228008d0a
Nishant <nishantyt0@gmail.com>
2024-03-18 21:05:31 +0545

    routes setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shreeantu_tea/screens/home_screen.dart';
+import 'package:shreeantu_tea/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      routes: AppRouter.routes,
+      initialRoute: AppRouter.initialRoute,
     );
   }
 }
