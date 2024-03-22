8090211cd4cbbb2d7a257aae809ec41137eaefea
i-maple <nishantyt0@gmail.com>
2024-03-22 15:42:20 +0000

    just checking what the fucking issue is

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "c93ea7bb247205020e40ef1274778357",
-"/": "c93ea7bb247205020e40ef1274778357",
+const RESOURCES = {"index.html": "b72f15e1e4665f45608d8447904e5194",
+"/": "b72f15e1e4665f45608d8447904e5194",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
@@ -36,7 +36,7 @@ const RESOURCES = {"index.html": "c93ea7bb247205020e40ef1274778357",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "e70fcbab8520a14d720e13b2aed4ae41",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
-"main.dart.js": "11ca345a3fe9f3a2bb6b71ecb4805838"};
+"main.dart.js": "911cfa56e5336fd99833cb32b90ae55d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
